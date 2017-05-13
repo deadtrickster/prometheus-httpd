4c662fe92259fa691e61ac95da3b789683bcf56a
Ilya Khaprov <dead.trickster@gmail.com>
2017-05-13 10:58:35 +0300

    Bump to v2.0.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PrometheusHTTPD.Mixfile do
 
   def project do
     [app: :prometheus_httpd,
-     version: "2.0.0",
+     version: "2.0.1",
      deps: deps(),
      description: description(),
      package: package()]
